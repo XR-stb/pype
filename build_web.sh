d765dd9a5851cfbde73dcff8321b9a3b3dbc6238
BLUELOVETH <blueloveTH@foxmail.com>
2023-05-04 18:28:18 +0000

    ...

--- build_web.sh
+++ build_web.sh
@@ -2,8 +2,5 @@ rm -rf build/web
 mkdir -p build/web
 cp platform/webassembly/index.html build/web
 cd build/web
-cmake -DCMAKE_TOOLCHAIN_FILE=/workspaces/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake \
-       -DCMAKE_BUILD_TYPE=Release \
-        ../../CMakeLists.txt
-
+emcmake cmake ../../CMakeLists.txt
 emmake make
