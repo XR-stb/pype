165adf7b1b9af4edd999c1e96ea0cc3e854cc6df
BLUELOVETH <blueloveTH@foxmail.com>
2023-05-04 19:06:33 +0000

    ...

--- build_web.sh
+++ build_web.sh
@@ -2,7 +2,12 @@ python preprocess.py
 rm -rf build/web
 mkdir -p build/web
 cp platform/webassembly/index.html build/web
+cp -r game build/web
 cd build/web
 emcmake cmake ../../CMakeLists.txt
 emmake make
 rm -rf CMakeFiles
+rm -rf CMakeCache.txt
+rm -rf Makefile
+rm -rf cmake_install.cmake
+rm -rf game
